3641b89b61fe4e2b813402916d95fb1342e82394
Joshua Justo <jjustoe04@gmail.com>
2024-03-01 09:36:36 +0800

    use axios

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-import puppeteer from "puppeteer";
-
-const getRank = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  var player_id = "380175663";
-  await page.goto(`https://www.dotabuff.com/players/${player_id}`, {
-    waitUntil: "domcontentloaded",
-  });
-  const rank = await page.evaluate(() =>
-    document.querySelector(".rank-tier-wrapper").getAttribute("oldtitle")
-  );
-  console.log(rank);
-  await browser.close();
-};
-getRank();
+import axios from "axios";
+import * as cheerio from "cheerio";
+var player_id = "380175663";
+axios.get(`https://www.dotabuff.com/players/380175663`).then((response)=>{
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const title = $(".rank-tier-wrapper").attr("title");
+    console.log(title);
+}).catch(console.error);
